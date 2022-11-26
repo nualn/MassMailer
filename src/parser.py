b93b2bdbc767c3975c47172f346b2a7728cc8b43
Nuutti Nikkola <nuuttinikkola1@gmail.com>
2022-11-26 18:35:18 +0200

    Modify parser

--- src/parser.py
+++ src/parser.py
@@ -1,10 +1,12 @@
 
 class _Parser:
 
-    def parse(self, text, variable_dict):
+    def parse(self, text, variable_keys, variable_values):
         result = text
-        for key in variable_dict.keys():
-            result = result.replace(f"[{key}]", str(variable_dict[key]))
+        for i, key in enumerate(variable_keys):
+            if i >= len(variable_values):
+                break
+            result = result.replace(f"[{key}]", str(variable_values[i]))
         return result
 
 parser = _Parser()

361c61f2ef69ed75f0beb358c7b0fe086f7fe379
Nuutti Nikkola <nuuttinikkola1@gmail.com>
2022-12-04 16:23:20 +0200

    Add to and subject fields to UI

--- src/ui/message_field.py
+++ src/ui/message_field.py
@@ -6,6 +6,17 @@ class Message_field:
         self._root = root
         self._frame = ttk.Frame(self._root)
 
+        to_label = ttk.Label(self._frame, text="To")
+        to_label.pack()
+        
+        self.to_entry = ttk.Entry(self._frame, width=40, font=("Arial",15))
+        self.to_entry.pack()
+
+        subj_label = ttk.Label(self._frame, text="Subject")
+        subj_label.pack()
+        
+        self.subject_entry = ttk.Entry(self._frame, width=40, font=("Arial",15))
+        self.subject_entry.pack()
         self.message_entry = scrolledtext.ScrolledText(
             self._frame,
             wrap=WORD,
@@ -17,7 +28,11 @@ class Message_field:
         self.message_entry.pack(pady=20)
 
     def get_message(self):
-        return self.message_entry.get("1.0", "end-1c")
+        return {
+            "to": self.to_entry.get(),
+            "subject": self.subject_entry.get(),
+            "body": self.message_entry.get("1.0", "end-1c")
+        }
 
     def pack(self):
         self._frame.pack()
--- src/ui/preview.py
+++ src/ui/preview.py
@@ -1,4 +1,4 @@
-from tkinter import ttk, WORD, scrolledtext
+from tkinter import ttk, WORD, scrolledtext, END
 
 
 class Preview:
@@ -8,6 +8,18 @@ class Preview:
         self.get_variables = get_variables
         self._parser = parser
         self._frame = ttk.Frame(self._root)
+
+        to_label = ttk.Label(self._frame, text="To")
+        to_label.pack()
+
+        self.preview_to = ttk.Entry(self._frame, width=40, font=("Arial",15), state="disabled")
+        self.preview_to.pack()
+
+        subj_label = ttk.Label(self._frame, text="Subject")
+        subj_label.pack()
+
+        self.preview_subject = ttk.Entry(self._frame, width=40, font=("Arial",15), state="disabled")
+        self.preview_subject.pack()
 
         self.preview_text = scrolledtext.ScrolledText(
             self._frame,
@@ -25,12 +37,23 @@ class Preview:
         preview_button.pack()
 
     def get_preview(self):
+        message = self.get_message()
+        variables = self.get_variables()
+
+        self.preview_to.configure(state="normal")
+        self.preview_subject.configure(state="normal")
         self.preview_text.configure(state="normal")
+
+        self.preview_to.delete(0, END)
+        self.preview_subject.delete(0, END)
         self.preview_text.delete("1.0", "end-1c")
-        self.preview_text.insert("1.0", self._parser.parse(
-            self.get_message(),
-            self.get_variables()
-        ))
+
+        self.preview_to.insert(0, self._parser.parse(message["to"], variables))
+        self.preview_subject.insert(0, self._parser.parse(message["subject"], variables))
+        self.preview_text.insert("1.0", self._parser.parse(message["body"], variables))
+
+        self.preview_to.configure(state="disabled")
+        self.preview_subject.configure(state="disabled")
         self.preview_text.configure(state="disabled")
 
     def pack(self):

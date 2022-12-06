8541058412caeb5fcd0c49f76adc3c8c0c0357b1
Nuutti Nikkola <nuuttinikkola1@gmail.com>
2022-12-06 21:44:44 +0200

    Refactor authorizer login method

--- src/oauth/authorizer.py
+++ src/oauth/authorizer.py
@@ -16,21 +16,29 @@ class Authorizer:
     def is_authorized(self):
         return self.creds and self.creds.valid
 
+    def _load_creds(self):
+        self.creds = Credentials.from_authorized_user_file(
+            'token.json', SCOPES)
+
+    def _get_new_creds(self):
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json', SCOPES)
+        self.creds = flow.run_local_server(port=0)
+
+    def _save_creds(self):
+        with open('token.json', 'w', encoding="utf-8") as token:
+            token.write(self.creds.to_json())
+
     def login(self):
         try:
             if not self.creds and os.path.exists('token.json'):
-                self.creds = Credentials.from_authorized_user_file(
-                    'token.json', SCOPES)
+                self._load_creds()
             if not self.is_authorized():
                 if self.creds and self.creds.expired and self.creds.refresh_token:
                     self.creds.refresh(Request())
                 else:
-                    flow = InstalledAppFlow.from_client_secrets_file(
-                        'credentials.json', SCOPES)
-                    self.creds = flow.run_local_server(port=0)
-                # Save the credentials for the next run
-                with open('token.json', 'w', encoding="utf-8") as token:
-                    token.write(self.creds.to_json())
+                    self._get_new_creds()
+                self._save_creds()
         except Exception:
             self.logout()
 
